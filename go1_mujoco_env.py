faf555e977e2cdd17708ea9f3f846e06b5450407
Romain Salvi <salvir.main@gmail.com>
2025-06-09 14:55:44 +0200

    Update go1_mujoco_env.py

--- go1_mujoco_env.py
+++ go1_mujoco_env.py
@@ -326,8 +326,8 @@ class Go1MujocoEnv(MujocoEnv):
 
 
     def random_point(self):
-        x = np.random.uniform(-self.half_x/8, self.half_x/8)
-        y = np.random.uniform(-self.half_y/8, self.half_y/8) #was /4 every /10
+        x = np.random.uniform(-self.half_x/4, self.half_x/4)
+        y = np.random.uniform(-self.half_y/4, self.half_y/4) #was /4 every /10
         
         return [x,y]
 
